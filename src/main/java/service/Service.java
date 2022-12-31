f4083d57f7796025e5f2660f8de537368ae618fb
rostislavdolbilov <rostislavdolbilov@gmail.com>
2022-12-31 20:39:06 +0200

    fixing problems

--- src/main/java/service/Service.java
+++ src/main/java/service/Service.java
@@ -1,14 +1,7 @@
 package service;
 
 import model.Transaction;
-import utils.CsvJsonReader;
-import utils.JsonObjectMapper;
-import utils.ObjectJsonMapper;
-
 import java.io.IOException;
-import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
 
 public interface Service {
     public Transaction getTransactionById(String id) throws IOException;
